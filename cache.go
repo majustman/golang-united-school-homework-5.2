b30ad5d8e42b6fda0feb93de4a2862ddec189786
samatt90 <samatt90@gmail.com>
2022-06-28 14:15:19 +0600

    init: solution

---
+++ cache.go
@@ -0,0 +1,43 @@
+package cache
+
+import "time"
+
+type Cache struct {
+	data    map[string]string
+	expData map[string]time.Time
+}
+
+func NewCache() Cache {
+	data := make(map[string]string)
+	expData := make(map[string]time.Time)
+	return Cache{data: data, expData: expData}
+}
+
+func (c *Cache) Get(key string) (string, bool) {
+	t, ok := c.expData[key]
+	if ok && t.Unix() < time.Now().Unix() {
+		return "", false
+	}
+	v, ok := c.data[key]
+	if !ok {
+		return "", false
+	}
+	return v, true
+}
+
+func (c *Cache) Put(key, value string) {
+	c.data[key] = value
+}
+
+func (c *Cache) Keys() []string {
+	var res []string
+	for key := range c.data {
+		res = append(res, key)
+	}
+	return res
+}
+
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.data[key] = value
+	c.expData[key] = deadline
+}
